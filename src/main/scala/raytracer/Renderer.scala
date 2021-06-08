e410c4c61d2121ea4f3e38ec126fa348ecb7e5bb
Nikolay Danailov <frostblooded@yahoo.com>
2021-06-08 21:23:23 +0300

    Add getting average of multiple samples

--- src/main/scala/raytracer/Renderer.scala
+++ src/main/scala/raytracer/Renderer.scala
@@ -2,6 +2,7 @@ package raytracer
 
 import breeze.linalg.{DenseVector, normalize}
 import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.color.{RGBColor, X11Colorlist}
 import com.sksamuel.scrimage.pixels.Pixel
 
 object Renderer {
@@ -10,14 +11,40 @@ object Renderer {
 
   def renderSettings: RenderSettings = RenderSettings(400, 300)
 
+  def pixelsAverage(pixels: List[Pixel]): Pixel = {
+    if(pixels.isEmpty) new Pixel(-1, -1, X11Colorlist.Purple.toARGBInt)
+
+    // Xs and Ys of all of the pixels in the list should be the same
+    val x = pixels.head.x
+    val y = pixels.head.y
+
+    val colors = pixels.map(_.toColor.toRGB)
+
+    val rs = colors.map(_.red)
+    val gs = colors.map(_.green)
+    val bs = colors.map(_.blue)
+
+    val rsAverage = rs.sum / rs.size
+    val gsAverage = gs.sum / gs.size
+    val bsAverage = bs.sum / bs.size
+
+    new Pixel(x, y, new RGBColor(rsAverage, gsAverage, bsAverage).toARGBInt)
+  }
+
   def render(scene: Scene, camera: Camera): ImmutableImage = {
-    val pixels: Array[Pixel] = 0.until(renderSettings.height).toArray.flatMap(y => {
+    val samplesCount = 5
+
+    val pixels: Array[List[Pixel]] = 0.until(renderSettings.height).toArray.flatMap(y => {
+      println(s"Col $y of ${renderSettings.height} done!")
+
       0.until(renderSettings.width).toArray.map(x => {
-        val color = scene.trace(camera.spawnRay(x, y))
-        color.toPixel(x, y)
+        0.until(samplesCount).map(i => {
+          scene.trace(camera.spawnRay(x, y)).toPixel(x, y)
+        }).toList
       })
     })
 
-    ImmutableImage.create(renderSettings.width, renderSettings.height, pixels)
+    val averagedPixels = pixels.map(pixelsAverage)
+    ImmutableImage.create(renderSettings.width, renderSettings.height, averagedPixels)
   }
 }
